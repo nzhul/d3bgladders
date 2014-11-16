a6734ebbbe79181ba00cc0d4add8f3796c230767
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-11-16 16:53:06 +0200

    Index page now works - almost

--- D3-API-by-ZTn/Application.Web/Controllers/HeroesController.cs
+++ D3-API-by-ZTn/Application.Web/Controllers/HeroesController.cs
@@ -22,8 +22,8 @@ namespace Application.Web.Controllers
         public JsonResult ReadAllHeroes([DataSourceRequest] DataSourceRequest request)
         {
             var result = this.Data.Heroes.All()
-                .OrderByDescending(x => x.Stats.Damage)
-                .ThenBy(x => x.Stats.Toughness)
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x =>x.Hardcore != true)
                 .Select(x => new HeroViewModel
             {
                 ID = x.ID,
@@ -42,9 +42,309 @@ namespace Application.Web.Controllers
             return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult ReadAllHeroesHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x =>x.ParagonLevel)
+                .Where(x => x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesBarbarian([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Barbarian && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesBarbarianHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Barbarian && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesWizard([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Wizard && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesWizardHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Wizard && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesDemonHunter([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.DemonHunter && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesDemonHunterHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.DemonHunter && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesMonk([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Monk && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesMonkHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Monk && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesWitchDoctor([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.WitchDoctor && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesWitchDoctorHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.WitchDoctor && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ReadAllHeroesCrusader([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Crusader && x.Hardcore != true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReadAllHeroesCrusaderHardcore([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = this.Data.Heroes.All()
+                .OrderBy(x => x.ParagonLevel)
+                .Where(x => x.HeroClass == HeroClass.Crusader && x.Hardcore == true)
+                .Select(x => new HeroViewModel
+                {
+                    ID = x.ID,
+                    BattleTag = x.ApplicationUser.BattleTag,
+                    Damage = x.Stats.Damage,
+                    Healing = x.Stats.Healing,
+                    HeroClass = x.HeroClass,
+                    IsHardcore = x.Hardcore,
+                    Life = x.Stats.Life,
+                    Name = x.Name,
+                    ParagonLevel = x.ParagonLevel,
+                    Toughness = x.Stats.Toughness
+
+                });
+
+            return Json(result.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Details(int id)
         {
+            var currentUserId = this.User.Identity.GetUserId();
             var viewModel = this.Data.Heroes.Find(id);
+            ViewBag.CanVote = !this.Data.HeroVotes.All().Any(x => x.HeroId == id && x.VotedById == currentUserId);
             return View(viewModel);
         }
 
@@ -77,6 +377,26 @@ namespace Application.Web.Controllers
                 // HttpResponceMessage needs using: using System.Net.Http;
                 return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, ModelState.Values.First().ToString());
             }
+        }
+
+        public ActionResult Vote(int id)
+        {
+            var userId = this.User.Identity.GetUserId();
+            var canVote = !this.Data.HeroVotes.All().Any(x => x.HeroId == id && x.VotedById == userId);
+            if (canVote)
+            {
+                this.Data.Heroes.Find(id).Votes.Add(new HeroVote
+                {
+                    HeroId = id,
+                    VotedById = userId
+                });
+
+                this.Data.SaveChanges();
+            }
+
+            var votes = this.Data.Heroes.Find(id).Votes.Count();
+
+            return Content(votes.ToString());
         }
     }
 }
